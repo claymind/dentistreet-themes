a61286257e089e904014909fd3f05d20e32f4eef
Preety chaudhary <preety.chaudhary03@gmail.com>
2018-05-24 17:30:29 +0000

    animation.js edited online with Bitbucket

--- theme-15/content/js/animation.js
+++ theme-15/content/js/animation.js
@@ -4,19 +4,19 @@
 
 
   // Window Scroll
-  var windowScroll = function() {
+  var windowScroll = function () {
     var lastScrollTop = 0;
-    $(window).scroll(function(event){
+    $(window).scroll(function (event) {
       var header = $('#fh5co-header'),
         scrlTop = $(this).scrollTop();
-      if ( scrlTop > 150 && scrlTop <= 1000 ) {
+      if (scrlTop > 150 && scrlTop <= 1000) {
         header.addClass('navbar-fixed-top fh5co-animated slideInDown');
-      } else if ( scrlTop <= 150) {
-        if ( header.hasClass('navbar-fixed-top') ) {
+      } else if (scrlTop <= 150) {
+        if (header.hasClass('navbar-fixed-top')) {
           header.addClass('navbar-fixed-top fh5co-animated slideOutUp');
-          setTimeout(function(){
+          setTimeout(function () {
             header.removeClass('navbar-fixed-top fh5co-animated slideInDown slideOutUp');
-          }, 100 );
+          }, 100);
         }
       }
     });
@@ -32,7 +32,7 @@
 
   });
 
- function determineDropDirection() {
+  function determineDropDirection() {
     $(".dropdown-menu").each(function () {
       $(this).css({
         visibility: "hidden",
@@ -53,13 +53,9 @@
 
   $(window).scroll(determineDropDirection);
 
+
   // Document on load.
-  $(function(){
-
-
+  $(function () {
     windowScroll();
-
   });
-
-
 }());
