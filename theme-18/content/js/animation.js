05aeaa324415273afdfc4cfb71498c9c2e4587e3
Preety chaudhary <preety.chaudhary03@gmail.com>
2018-07-04 09:55:58 +0000

    animation.js edited online with Bitbucket

--- theme-18/content/js/animation.js
+++ theme-18/content/js/animation.js
@@ -22,58 +22,6 @@
         });
     };
 
-    $(document).scroll(function () {
-        var y = $(this).scrollTop();
-        if (y > 400) {
-            $('.bottom-fixed').fadeIn();
-        } else {
-            $('.bottom-fixed').fadeOut();
-        }
-    });
-
-    $('#goTop').on('click', function (e) {
-        $("html, body").animate({scrollTop: $("#top").offset().top}, 500);
-        return false;
-    });
-
-    var searchIcon = function () {
-        $(document).on('click', function (e) {
-            var clickID = e.target.id;
-            if ((clickID !== 'input-search')) {
-                $('.header-search-form').removeClass('show');
-            }
-        });
-
-        $('.search').on('click', function (event) {
-            event.stopPropagation();
-        });
-
-        $('.header-search-form').on('click', function (event) {
-            event.stopPropagation();
-        });
-
-        $('.search').on('click', function (event) {
-            if (!$('.header-search-form').hasClass("show")) {
-                $('.header-search-form').addClass('show');
-                event.preventDefault();
-            }
-
-            else
-                $('.header-search-form').removeClass('show');
-            event.preventDefault();
-
-        });
-    };
-
-    $(document).ready(function () {
-        $(".dropdown, .btn-group").hover(function () {
-            var dropdownMenu = $(this).children(".dropdown-menu");
-            if (dropdownMenu.is(":visible")) {
-                dropdownMenu.parent().toggleClass("open");
-            }
-        });
-    });
-
     var isMobile = {
         Android: function () {
             return navigator.userAgent.match(/Android/i);
@@ -156,7 +104,6 @@
     $(function () {
         fullHeight();
         windowScroll();
-        searchIcon();
         burgerMenu();
         mobileMenuOutsideClick();
     });
