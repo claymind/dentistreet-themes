ee5a4d6c29405299634e318ec36a0a9c289020e7
Preety chaudhary <preety.chaudhary03@gmail.com>
2018-07-04 07:36:19 +0000

    animation.js edited online with Bitbucket

--- theme-17/content/js/animation.js
+++ theme-17/content/js/animation.js
@@ -3,20 +3,21 @@
   'use strict';
 
 
+
   // Window Scroll
-  var windowScroll = function() {
+  var windowScroll = function () {
     var lastScrollTop = 0;
-    $(window).scroll(function(event){
+    $(window).scroll(function (event) {
       var header = $('#fh5co-header'),
         scrlTop = $(this).scrollTop();
-      if ( scrlTop > 150 && scrlTop <= 1000 ) {
+      if (scrlTop > 150 && scrlTop <= 1000) {
         header.addClass('navbar-fixed-top fh5co-animated slideInDown');
-      } else if ( scrlTop <= 150) {
-        if ( header.hasClass('navbar-fixed-top') ) {
+      } else if (scrlTop <= 150) {
+        if (header.hasClass('navbar-fixed-top')) {
           header.addClass('navbar-fixed-top fh5co-animated slideOutUp');
-          setTimeout(function(){
+          setTimeout(function () {
             header.removeClass('navbar-fixed-top fh5co-animated slideInDown slideOutUp');
-          }, 100 );
+          }, 100);
         }
       }
     });
@@ -25,22 +26,20 @@
   $(document).scroll(function () {
     var y = $(this).scrollTop();
     if (y > 400) {
-      $('.sticky-book').fadeIn();
+      $('.bottom-fixed').fadeIn();
     } else {
-      $('.sticky-book').fadeOut();
+      $('.bottom-fixed').fadeOut();
     }
-
   });
 
+  $('#goTop').on('click', function (e) {
+    $("html, body").animate({ scrollTop: $("#top").offset().top }, 500);
+    return false;
+  });
 
 
   // Document on load.
-  $(function(){
-
-
+  $(function () {
     windowScroll();
-
   });
-
-
 }());
